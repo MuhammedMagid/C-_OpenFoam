528036e58b89cf0c5d18b159cf54979f73ff39a8
MuhammedMagid <muhammedmagid@gmail.com>
2024-09-26 16:53:35 +0300

    functions added

--- basic/dimensionlessNumbers.cpp
+++ basic/dimensionlessNumbers.cpp
@@ -4,13 +4,23 @@ using namespace std;
 #include<string>
 #include <map>
 
+	double Reynolds (double u, double l, double rho, double mu);
+	double Prandtl (double C_p, double mu, double k);
+	double Nusselt (double h, double l, double k );
+	double Sherwood (double h_d, double l, double d);
+	double Froude (double u, double l);
+	double Grashof (double nu , double l, double beta, double deltaT);
+	double Mach (double u, double v);
+	double Schmidt (double rho, double mu, double D);
 
 	int main()
 	{
 		string  dash = "*----------------------------------*";
 		
+		double rho  , u , l , mu , nu, c_p , k , h , d, h_d, v , D , deltaT;
+		
 		map<string, string> symbol  = {
-			{"Re","Reynolds number"},{"u", "Velocity"}, {"rho ", "Density"},{"L", "Characteristic length"}, {"mu","Dynamic viscosity"}
+			{"Re","Reynolds number"},{"u", "Velocity"}, {"rho ", "Density"},{"l", "Characteristic length"}, {"mu","Dynamic viscosity"}
 			};
 
 		int dnumber;
@@ -29,26 +39,25 @@ using namespace std;
 			
 		//Reynolds number case
 		case 01:
-			
-			float rho  , u , L , mu;
-			
+		
 			//details
 			cout <<"Eqn in LaTex: Re = \\frac{\\rho  u L}{\\mu}" <<endl;
 			cout<< "Where" <<endl;
 			cout<<"Re:	 " <<symbol["Re"] <<endl;
 			cout<<"rho:	 " <<symbol["rho "] <<endl;
 			cout<<"u:	 " <<symbol["u"] <<endl;
-			cout<<"L:	 " <<symbol["L"] <<endl;
+			cout<<"L:	 " <<symbol["l"] <<endl;
 			cout<<"mu:	 " <<symbol["mu"] <<endl;
 			cout << dash <<endl;
 			
 			//input data
 			cout << "Enter values in order [rho , u , L , mu]"<<endl;;
-			cin >> rho >> u >>L >> mu;
+			cin >> rho >> u >>l >> mu;
 			cout << dash <<endl;
 			
 			//caculation and print
-			cout << "Re = " << (rho *u*L)/mu <<endl;
+			
+			cout << Reynolds(u,l,rho,mu)<<endl;
 			
 			cout << dash <<endl;
 			cout << "End of Program"<<endl;
@@ -60,7 +69,17 @@ using namespace std;
 			break;
 		default:
 			cout << "Enter a valid CODE";
-	}
-
 		}
 
+	}
+
+	double Reynolds (double u, double l, double rho, double mu){
+		return (u*l*rho)/mu ;
+	};
+	double Prandtl (double C_p, double mu, double k);
+	double Nusselt (double h, double l, double k );
+	double Sherwood (double h_d, double l, double d);
+	double Froude (double u, double l);
+	double Grashof (double nu , double l, double beta, double deltaT);
+	double Mach (double u, double v);
+	double Schmidt (double rho, double mu, double D);
